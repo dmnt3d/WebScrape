9e4f6e08e06ca59a46d40e7d99496660ce45ce65
dmnt3d <root@localhost.localdomain>
2016-06-05 04:24:44 -0700

    test

---
+++ Scrape.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+#import urllib
+import requests
+
+BLOG  =  'http://blogs.vmware.com/vcloud'
+#url =  urllib.urlopen('http://blogs.vmware.com/vcloud')
+r = requests.get(BLOG)
+data = r.text
+soup = BeautifulSoup(data)
+
+soup
+
+print soup
